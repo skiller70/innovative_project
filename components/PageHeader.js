7105601869292de5dd2ea99884be7b4fe9d9a430
lucky <singhlucky007007@gmail.com>
2022-11-22 15:40:37 +0530

    done error

--- components/PageHeader.js
+++ components/PageHeader.js
@@ -1,12 +1,11 @@
 
 const PageHeader = (props) => {
-    console.log(props)
-    const {pTitle1} = props.pageTitle;
+
     return (
         <div className=" px-[21.19em] w-[100%] relative -top-[10rem]">
             
             <div className="   bg-white  flex  h-[19.31rem]  justify-center items-center shadow-xl rounded-[2rem]">
-            <h1 className=" font-[31.25rem] text-[4.50rem]  font-bodyCopy ">{pTitle1}</h1> 
+            <h1 className=" font-[31.25rem] text-[4.50rem]  font-bodyCopy ">{props.pageTitle.pTitle1}</h1> 
             </div>
 
           
