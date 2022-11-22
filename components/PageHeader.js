203c743391186f1addad09f6a156e34a90036fa9
lucky <singhlucky007007@gmail.com>
2022-11-22 13:54:17 +0530

    fix

--- components/PageHeader.js
+++ components/PageHeader.js
@@ -5,7 +5,7 @@ const PageHeader = (props) => {
         <div className=" px-[21.19em] w-[100%] relative -top-[10rem]">
             
             <div className="   bg-white  flex  h-[19.31rem]  justify-center items-center shadow-xl rounded-[2rem]">
-            <h1 className=" font-[31.25rem] text-[4.50rem]  font-bodyCopy ">{title1}</h1> 
+            <h1 className=" font-[31.25rem] text-[4.50rem]  font-bodyCopy ">{pageTitle1}</h1> 
             </div>
 
           
